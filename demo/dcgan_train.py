dc674acf27f6f89c0543926eab4fbf211540f41b
xschen <xs0040@gmail.com>
2018-02-14 15:13:04 +0800

    fix the missing image_utils.py

---
+++ demo/dcgan_train.py
@@ -0,0 +1,40 @@
+from keras_text_to_image.library.dcgan import DCGan
+from keras_text_to_image.library.utility.img_cap_loader import load_img_cap
+import numpy as np
+from random import shuffle
+
+
+def main():
+    seed = 42
+
+    np.random.seed(seed)
+
+    img_dir_path = './data/pokemon/img'
+    txt_dir_path = './data/pokemon/txt'
+    model_dir_path = './models/pokemon'
+    image_label_pairs = load_img_cap(img_dir_path, txt_dir_path)
+
+    shuffle(image_label_pairs)
+
+    img_width = 32
+    img_height = 32
+    img_channels = 3
+
+    gan = DCGan()
+    gan.img_width = img_width
+    gan.img_height = img_height
+    gan.img_channels = img_channels
+    gan.random_input_dim = 200
+    gan.glove_source_dir_path = './very_large_data'
+
+    batch_size = 4
+    epochs = 100
+    gan.fit(model_dir_path=model_dir_path, image_label_pairs=image_label_pairs,
+            snapshot_dir_path='./data/outputs',
+            snapshot_interval=100,
+            batch_size=batch_size,
+            epochs=epochs)
+
+
+if __name__ == '__main__':
+    main()
---
+++ keras_text_to_image/library/utility/image_utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+import math
+
+
+def combine_images(generated_images):
+    num = generated_images.shape[0]
+    width = int(math.sqrt(num))
+    height = int(math.ceil(float(num) / width))
+    shape = generated_images.shape[1:]
+    image = np.zeros((height * shape[0], width * shape[1], shape[2]),
+                     dtype=generated_images.dtype)
+    for index, img in enumerate(generated_images):
+        i = int(index / width)
+        j = index % width
+        image[i * shape[0]:(i + 1) * shape[0], j * shape[1]:(j + 1) * shape[1], :] = img
+    return image
